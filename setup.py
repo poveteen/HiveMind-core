2f28c3fa7d7e42ffe8c9c79bdb9c68e2473bb28b
jarbasai <jarbasai@mailfence.com>
2022-04-25 15:48:32 +0100

    automations and workflows

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 def get_version():
     """ Find the version of the package"""
     version = None
-    version_file = os.path.join(BASEDIR, 'ovos_config_assistant', 'version.py')
+    version_file = os.path.join(BASEDIR, 'jarbas_hive_mind', 'version.py')
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
